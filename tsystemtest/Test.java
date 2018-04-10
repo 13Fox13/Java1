85c198b9b5180bc79abed7a669121bd2d0ca8d2a
Andrey Klopov <13-fox@mail.ru>
2018-04-10 20:48:30 +0300

    Update package

--- tsystemtest/Test.java
+++ tsystemtest/Test.java
@@ -3,11 +3,11 @@ package tsystemtest;
 public class Test {
     public static void main(String[] args) {
         // Task 1. Calculator
-        Calculator c = new CalculatorImpl();
+        tsystemtest.Calculator c = new tsystemtest.CalculatorImpl();
         System.out.println(c.evaluate("(1+38)*4-5")); // Result: 151
-        c = new CalculatorImpl();
+        c = new tsystemtest.CalculatorImpl();
         System.out.println(c.evaluate("7*6/2+8")); // Result: 29
-        c = new CalculatorImpl();
+        c = new tsystemtest.CalculatorImpl();
         System.out.println(c.evaluate("-12)1//(")); // Result: null
         System.out.println();
 
