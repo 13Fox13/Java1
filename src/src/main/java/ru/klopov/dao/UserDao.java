3580fd8afa7659de8de623ffea69d09f78306859
Andrey Klopov <13-fox@mail.ru>
2018-06-15 14:51:58 +0300

    Add easy web app Spring MVC

---
+++ src/src/main/java/ru/klopov/dao/UserDao.java
@@ -0,0 +1,19 @@
+package ru.klopov.dao;
+
+import java.util.List;
+
+import ru.klopov.model.User;
+
+public interface UserDao {
+ 
+ public List listAllUsers();
+ 
+ public void addUser(User user);
+ 
+ public void updateUser(User user);
+ 
+ public void deleteUser(int id);
+ 
+ public User findUserById(int id);
+ 
+}
